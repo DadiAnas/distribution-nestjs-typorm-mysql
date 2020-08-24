ef85a9e30146330a19ee3af41d52057f8d066406
Anas Dadi <dadi.anas@hotmail.fr>
2020-08-24 03:13:34 +0100

    p

--- generate.bat
+++
@@ -1,8 +0,0 @@
-set fileName=%1
-set className=%2
-set sourcefolder=%cd%\src
-set schemaFolder=%sourcefolder%\schama
-
-mkdir %schemaFolder%
-touch %schemaFolder%\%fileName%.schema.ts
-echo export Create%className%{} export Update%className%{} > %schemaFolder%/%fileName%.dto.ts
